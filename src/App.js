9053e968f8b2429d3c1c83e2ceab94794b551b9f
Alhassan Joshua Abel <saintjosh26@gmail.com>
2023-03-10 00:29:11 +0000

    testing the navigation bar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import Home from "./routes/home/Home.component";
-import {Routes, Route} from 'react-router-dom';
+import {Routes, Route, Outlet} from 'react-router-dom';
+
+const Navigation = () => {
+  return (
+    <div>
+      <h1>I am the Navigation Bar</h1>
+      <Outlet />
+    </div>
+  )
+}
+
+// Shop component
+const Shop = () => {
+  return <h1>I am the Shop page</h1>
+}
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index={true} element={<Home/>} />
+        <Route path="Shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 }
