607b6838ce7a7bdfd960da2a1910eeb73a3cb8b5
Alhassan Joshua Abel <saintjosh26@gmail.com>
2023-03-11 00:21:49 +0000

    setup authentication with firebase libraries

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -41,6 +41,22 @@ export const createUserDocumentFromAuth = async(userAuth) => {
   console.log(userDocRef);
 
   const userSnapshot = await getDoc(userDocRef);
-  console.log(userSnapshot);
-  console.log(userSnapshot.exists());
+  
+  // Creating user Document
+  if (!userSnapshot.exists()) {
+    const {displayName, email} = userAuth;
+    const createdAt = new Date();
+  
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch(error) {
+      console.log("Error creating the user", error.message);
+    }
+  }
+
+  return userDocRef;
 }
