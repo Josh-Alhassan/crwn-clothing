bcea8fb67d53555ebb11f560a9d9130f2853bf1e
Alhassan Joshua Abel <saintjosh26@gmail.com>
2023-03-11 13:29:26 +0000

    sign-up-form

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -3,6 +3,8 @@ import {
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+
 // import { async } from "@firebase/util";
 
 const SignIn = () => {
@@ -15,6 +17,8 @@ const SignIn = () => {
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+
+      <SignUpForm />
     </div>
   );
 };
